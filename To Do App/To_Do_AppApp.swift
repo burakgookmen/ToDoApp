ee69d74930a0af7cc692f6d4a77da2f6b48da5ec
Burak <146767454+burakgookmen@users.noreply.github.com>
2024-11-21 11:39:47 +0300

    Initial Commit

---
+++ To Do App/To_Do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppApp.swift
+//  To Do App
+//
+//  Created by Burak on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ To Do AppTests/To_Do_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppTests.swift
+//  To Do AppTests
+//
+//  Created by Burak on 21.11.2024.
+//
+
+import Testing
+@testable import To_Do_App
+
+struct To_Do_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
